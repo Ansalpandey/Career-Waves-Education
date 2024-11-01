9a98cb105743fd31e2fdaca0473505d3c8663386
Ansalpandey <pandeyansal77@gmail.com>
2024-11-01 23:29:25 +0530

    Update Azure workflow configuration, add VSCode settings, and enhance server setup with dotenv

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const dotenv = require("dotenv")
+dotenv.config()
 
 // Serve static files from the Vite build directory located in the client/dist folder
 app.use(express.static(path.join(__dirname, '../client', 'dist')));
